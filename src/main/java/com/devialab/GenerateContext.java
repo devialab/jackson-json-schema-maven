1923a70bca4af2d358ddf6f2821422abb369575b
Alberto J. Rubio <yebenes9@gmail.com>
2017-09-20 17:06:20 +0200

    Use json-schema v4

--- src/main/java/com/devialab/GenerateContext.java
+++ src/main/java/com/devialab/GenerateContext.java
@@ -3,9 +3,7 @@ package com.devialab;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
-import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+import com.kjetland.jackson.jsonSchema.JsonSchemaGenerator;
 import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -22,14 +20,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@Mojo(name="generate-context")
-public class GenerateContext extends AbstractMojo
-{
-    @Parameter(required = true)
-    private File outputDirectory;
+@Mojo(name = "generate-context")
+public class GenerateContext extends AbstractMojo {
+
+    private static final String JSON_EXTENSION = ".json";
 
     @Parameter(required = true)
-    private File fileName;
+    private File outputDirectory;
 
     @Parameter(required = true)
     private String packageName;
@@ -40,35 +37,28 @@ public class GenerateContext extends AbstractMojo
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
     private MavenProject project;
 
-    public void execute() throws MojoExecutionException
-    {
-        try
-        {
+    public void execute() throws MojoExecutionException {
+        try {
             addProjectClasspath();
             ObjectMapper mapper = new ObjectMapper();
             JsonSchemaGenerator jsonSchemaGenerator = new JsonSchemaGenerator(mapper);
-            Path filePath = Paths.get(outputDirectory.getAbsolutePath(), fileName.getName());
-            if (!Files.exists(filePath))
-            {
-                Files.createFile(filePath);
-            }
-            ObjectNode jsonSchema = mapper.createObjectNode();
             new FastClasspathScanner(packageName).matchAllStandardClasses((clazz) -> {
                 try {
-                    JsonSchema schema = jsonSchemaGenerator.generateSchema(clazz);
-                    JsonNode jsonNode = mapper.readTree(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(schema));
-                    jsonSchema.set(clazz.getSimpleName(), jsonNode);
+                    JsonNode jsonNode = jsonSchemaGenerator.generateJsonSchema(clazz);
+                    if (jsonNode.has("type")) {
+                        String filename = clazz.getSimpleName().toLowerCase().concat(JSON_EXTENSION);
+                        Path filePath = Paths.get(outputDirectory.getAbsolutePath(), filename);
+                        if (!Files.exists(filePath)) Files.createFile(filePath);
+                        Files.write(filePath, mapper.writeValueAsBytes(jsonNode));
+                    }
                 } catch (JsonProcessingException e) {
                     getLog().error(new MojoExecutionException(e.getMessage(), e));
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     getLog().error(new MojoExecutionException("Error creating context file. Message: " + e.getMessage(), e));
                 }
             }).scan();
-            Files.write(filePath, mapper.writeValueAsBytes(jsonSchema));
-            getLog().info("Context file of package '"+ packageName +"' written to: " + filePath);
-        }
-        catch (IOException e) {
+            getLog().info("Json schemas of package '" + packageName + "' written to: " + outputDirectory.getAbsolutePath());
+        } catch (IOException e) {
             throw new MojoExecutionException("Error creating context file. Message: " + e.getMessage(), e);
         }
     }
