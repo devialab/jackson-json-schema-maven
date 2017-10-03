3b46a2a3fa8491ea74c21a9926984da6bd4114bc
Alberto J. Rubio <yebenes9@gmail.com>
2017-10-03 20:33:33 +0200

    Check class name is not empty

--- src/main/java/com/devialab/GenerateContext.java
+++ src/main/java/com/devialab/GenerateContext.java
@@ -44,12 +44,14 @@ public class GenerateContext extends AbstractMojo {
             JsonSchemaGenerator jsonSchemaGenerator = new JsonSchemaGenerator(mapper);
             new FastClasspathScanner(packageName).matchAllStandardClasses((clazz) -> {
                 try {
-                    JsonNode jsonNode = jsonSchemaGenerator.generateJsonSchema(clazz);
-                    if (jsonNode.has("type")) {
-                        String filename = clazz.getSimpleName().toLowerCase().concat(JSON_EXTENSION);
-                        Path filePath = Paths.get(outputDirectory.getAbsolutePath(), filename);
-                        if (!Files.exists(filePath)) Files.createFile(filePath);
-                        Files.write(filePath, mapper.writeValueAsBytes(jsonNode));
+                    if(!clazz.getSimpleName().isEmpty()) {
+                        JsonNode jsonNode = jsonSchemaGenerator.generateJsonSchema(clazz);
+                        if (jsonNode.has("type")) {
+                            String filename = clazz.getSimpleName().toLowerCase().concat(JSON_EXTENSION);
+                            Path filePath = Paths.get(outputDirectory.getAbsolutePath(), filename);
+                            if (!Files.exists(filePath)) Files.createFile(filePath);
+                            Files.write(filePath, mapper.writeValueAsBytes(jsonNode));
+                        }
                     }
                 } catch (JsonProcessingException e) {
                     getLog().error(new MojoExecutionException(e.getMessage(), e));
